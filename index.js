227261c96ca1241d895f991612fb652764dd720d
twhiting6 <timothy.whiting@chch.ox.ac.uk>
2017-04-11 14:01:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://amondo-parse-server-test+PARSE_MOUNT.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
-  verifyUserEmails: true
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
